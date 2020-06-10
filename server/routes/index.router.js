8a7ad48857bed5d7b83895799c503616e28dc997
Luong97vjppro <trantienluong97@gmail.com>
2020-06-10 19:58:37 +0700

    Add resource order at website API

--- server/routes/index.router.js
+++ server/routes/index.router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const ctrlUser = require('../controllers/user.controller');
 const productController = require("../controllers/product.controller");
 const cartController = require("../controllers/cart.controller");
+const orderController = require("../controllers/order.controller");
 
 const jwtHelper = require('../config/jwtHelper');
 
@@ -18,6 +19,9 @@ router.get("/cart", cartController.index)
 
 router.get("/products", productController.index)
     .get("/products/:id", productController.show);
+
+router.get("/orders", jwtHelper.verifyJwtToken, orderController.index)
+    .get("/orders/:id", jwtHelper.verifyJwtToken, orderController.show);
 
 module.exports = router;
 
